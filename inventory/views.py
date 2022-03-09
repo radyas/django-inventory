5bc5e49c4013dafcb2f4ef1e952e681c64c595e4
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-03-09 18:07:15 +0530

    feat: added urls and base views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class InventoryViewSet(ModelViewSet):
+    pass
+
+
+def inventory(request):
+    pass
+
+
+def inventory_detail(request, inventory_id):
+    pass
