7021a9704e8f9ab789884f2d32368f7ce97cfc89
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-03-09 21:09:15 +0530

    feat: updated inventory views

--- inventory/views.py
+++ inventory/views.py
@@ -1,14 +1,43 @@
+import requests
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
+from inventory.models import Inventory
+
 
 class InventoryViewSet(ModelViewSet):
-    pass
+    model = Inventory
 
 
 def inventory(request):
-    pass
+    data = []
+
+    # url to fetch data
+    url = f'http://{request.get_host()}/api/inventory'
+    response = requests.get(url)
+
+    # set data if the response is successful
+    if response.status_code == 200:
+        data = response.content
+
+    context = {
+        'items': data
+    }
+    return render(request, 'inventory.html', context)
 
 
 def inventory_detail(request, inventory_id):
-    pass
+    data = None
+
+    # url to fetch data
+    url = f'http://{request.get_host()}/api/inventory/{inventory_id}'
+    response = requests.get(url)
+
+    # set data if the response is successful
+    if response.status_code == 200:
+        data = response.content
+
+    context = {
+        'item': data
+    }
+    return render(request, 'inventory-detail.html', context)
