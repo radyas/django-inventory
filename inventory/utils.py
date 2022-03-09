503452ae58a3fbc67fe072c4229ad3640d0b8bb7
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-03-10 01:05:22 +0530

    fix: refactoring

--- inventory/utils.py
+++ inventory/utils.py
@@ -1,5 +1,31 @@
+from datetime import datetime
+
+import requests
 from rest_framework.utils import json
 
 
 def format_response_data(data):
     return json.loads(data)
+
+
+def get_list_data(url):
+    response = requests.get(url)
+
+    # set data if the response is successful
+    if response.status_code == 200:
+        return format_response_data(response.json())
+    else:
+        return []
+
+
+def get_detail_data(url):
+    response = requests.get(url)
+
+    # set data if the response is successful
+    if response.status_code == 200:
+        data = format_response_data(response.json())
+        data['added_at'] = datetime.fromisoformat(data['added_at'])
+        return data
+
+    else:
+        return None
--- inventory/views.py
+++ inventory/views.py
@@ -1,12 +1,9 @@
-from datetime import datetime
-
-import requests
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from inventory.models import Inventory
 from inventory.serializers import InventorySerializer, InventoryDetailSerializer
-from inventory.utils import format_response_data
+from inventory.utils import get_list_data, get_detail_data
 
 
 class InventoryViewSet(ModelViewSet):
@@ -21,35 +18,20 @@ class InventoryViewSet(ModelViewSet):
 
 
 def inventory(request):
-    data = []
-
     # url to fetch data
     url = f'http://{request.get_host()}/api/inventory'
-    response = requests.get(url)
-
-    # set data if the response is successful
-    if response.status_code == 200:
-        data = format_response_data(response.content)
 
     context = {
-        'items': data
+        'items': get_list_data(url)
     }
     return render(request, 'inventory.html', context)
 
 
 def inventory_detail(request, inventory_id):
-    data = None
-
     # url to fetch data
     url = f'http://{request.get_host()}/api/inventory/{inventory_id}'
-    response = requests.get(url)
-
-    # set data if the response is successful
-    if response.status_code == 200:
-        data = format_response_data(response.content)
-        data['added_at'] = datetime.fromisoformat(data['added_at'])
 
     context = {
-        'item': data
+        'item': get_detail_data(url)
     }
     return render(request, 'inventory-detail.html', context)
